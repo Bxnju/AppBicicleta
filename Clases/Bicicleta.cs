153b187768731df7be6c5efa271a169a3dc9a781
Bxnju <andres.benjumea@upb.edu.co>
2023-01-20 19:37:17 -0500

    Primer commit simulador de bicicleta C#

---
+++ Clases/Bicicleta.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBicicleta.Clases
+{
+    internal class Bicicleta
+    {
+        public string marca;
+        public string color;
+        public string material;
+        public byte nro_cambios = 5;
+        public char tam_marco;
+       
+        //Atributos de Comportamiento
+
+        public byte cambio_actual = 1;
+        public float velocidad_actual = 0;
+        
+        public bool subir_Cambio()
+        {
+            try
+            {
+
+                if (cambio_actual == nro_cambios)
+                {
+                    return false;
+                }else
+                {
+                    cambio_actual++;
+                    return true;
+                }
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool bajar_Cambio()
+        {
+            try
+            {
+
+                if (cambio_actual == 1) return false;
+                else
+                {
+                    cambio_actual--;
+                    return true;
+                }
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public void acelerar(float vel_objetivo)
+        {
+            try
+            {
+                Console.Write("Acelerando bicicleta...");
+
+                using (var progress = new ProgressBar())
+                {
+                    for (int i = 0; i <= 100; i++)
+                    {
+                        progress.Report((double)i / 100);
+                        Thread.Sleep(60);
+                    }
+                }
+
+                while (velocidad_actual < vel_objetivo)
+                {
+                    velocidad_actual++;
+                }
+
+                if (vel_objetivo == velocidad_actual)
+                {
+                    Console.WriteLine("\nLa bicicleta ha sido acelerada. Su nueva velocidad es: " + velocidad_actual);
+                }
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hubo un error acelerando la cicla");
+            }
+        }
+
+        public void frenar(float vel_objetivo)
+        {
+            try
+            {
+                if(!(vel_objetivo < 0))
+                {
+
+                    Console.Write("Frenando bicicleta...");
+
+                    using (var progress = new ProgressBar())
+                    {
+                        for (int i = 0; i <= 100; i++)
+                        {
+                            progress.Report((double)i / 100);
+                            Thread.Sleep(20);
+                        }
+                    }
+
+                    while (velocidad_actual > vel_objetivo)
+                    {
+                        velocidad_actual--;
+                    }
+
+                    if (vel_objetivo == velocidad_actual)
+                    {
+                        Console.WriteLine("\nLa bicicleta ha sido frenada. Su nueva velocidad es: " + velocidad_actual);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("La velocidad a la que se puede frenar no puede estar por debajo de cero.");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hubo un error frenando la cicla");
+            }
+        }
+
+        public void parar()
+        {
+            try
+            {
+                if (velocidad_actual != 0)
+                {
+                    Console.Write("Parando bicicleta... ");
+                    velocidad_actual = 0;
+
+                    using (var progress = new ProgressBar())
+                    {
+                        for (int i = 0; i <= 100; i++)
+                        {
+                            progress.Report((double)i / 100);
+                            Thread.Sleep(20);
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("La bicicleta ha sido parada en su totalidad.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("La bicicleta ya esta en reposo.");
+                    Console.ResetColor();
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hubo un error acelerando la cicla");
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using AppBicicleta.Clases;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+		try
+		{
+
+            byte vel_objetivo;
+            byte opcion = 0;
+            Bicicleta bicicleta = new Bicicleta();
+
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ResetColor();
+                Console.WriteLine("\nSimulador de Bicicleta hecho por Benju.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\nVelocidad actual de la bicicleta: " + bicicleta.velocidad_actual + "\nCambio actual de la bicicleta: " + bicicleta.cambio_actual);
+                Console.ResetColor();
+                Console.Write("\n\nSeleccione una de las siguientes opciones:\n1. Acelerar Bicicleta\n2. Frenar Bicicleta\n3. Subir cambio.\n4. Bajar cambio.\n5. Parar Bicicleta\n6. Salir del programa.");
+                Console.WriteLine();
+                opcion = Byte.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1: acelerar(); break;
+                    case 2: frenar(); break;
+                    case 3: subir(); break;
+                    case 4: bajar(); break;
+                    case 5: parar(); break;
+                    case 6:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nGracias por utilizar el programa.");
+                        Console.ResetColor();
+                    break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nSeleccione una de las opciones disponibles.");
+                        Console.ResetColor();
+                        break;
+                }
+
+
+            } while (opcion != 6);
+
+
+            void acelerar()
+            {
+                Console.WriteLine("Ingrese la velocidad hasta la cual desea acelerar.");
+                vel_objetivo = Byte.Parse(Console.ReadLine());
+                bicicleta.acelerar(vel_objetivo);
+            }
+
+            void frenar()
+            {
+                Console.WriteLine("Ingrese la velocidad hasta la que quiere frenar.");
+                vel_objetivo = Byte.Parse(Console.ReadLine());
+                bicicleta.frenar(vel_objetivo);
+            }
+
+            void subir()
+            {
+                if (bicicleta.subir_Cambio())
+                {
+                    Console.WriteLine("El cambio ha subido. En este momento esta en el cambio " + bicicleta.cambio_actual);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No ha sido posible subir el cambio ya que se encuentra en el " + bicicleta.cambio_actual);
+                    Console.ResetColor();
+                }
+            }
+
+            void bajar()
+            {
+                if (bicicleta.bajar_Cambio())
+                {
+                    Console.WriteLine("El cambio ha bajado. En este momento esta en el cambio " + bicicleta.cambio_actual);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nNo ha sido posible bajar el cambio ya que se encuentra en primera");
+                    Console.ResetColor();
+                }
+            }
+
+            void parar()
+            {
+                bicicleta.parar();
+            }
+
+
+        }
+        catch (Exception)
+		{
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\nOcurrio un error inesperado.");
+            Console.ResetColor();
+		}
+
+    }
+}
